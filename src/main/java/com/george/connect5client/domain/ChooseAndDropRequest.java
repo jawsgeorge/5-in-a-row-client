6580fd6a48fd5a98ae90044877c5da6b4d5b32f6
jawsgeorge <geostephenthomas@gmail.com>
2021-01-25 00:23:13 +0000

    Added code for connect 5 board game client

---
+++ src/main/java/com/george/connect5client/domain/ChooseAndDropRequest.java
@@ -0,0 +1,13 @@
+package com.george.connect5client.domain;
+
+public class ChooseAndDropRequest {
+    Integer column;
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
